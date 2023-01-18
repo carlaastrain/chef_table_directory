ea63c65e162332252aaf4f5b3b6d1acc964ea891
Carla Astrain <carlaastrain@gmail.com>
2023-01-17 20:54:28 -0800

    creating space between containers in details page

--- lib/screens/restaurant_details_screen.dart
+++ lib/screens/restaurant_details_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:gap/gap.dart';
 import 'package:get_it/get_it.dart';
 import 'package:my_app/services/restaurant_service.dart';
+import 'package:my_app/utils/app_layout.dart';
 
 import '../utils/app_styles.dart';
 import '../widgets/details_row.dart';
@@ -16,81 +17,100 @@ class RestaurantDetailsScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return FutureBuilder(
-        future: restaurantService.getRestaurant(restaurantId),
-        builder: (context, snapshot) {
-          if (!snapshot.hasData) {
-            return Scaffold(
-                appBar: AppBar(
-                  backgroundColor: Colors.indigo[400],
-                ),
-                body: const Center(child: CircularProgressIndicator()));
-          }
-
-          final restaurant = snapshot.data!;
-
+      future: restaurantService.getRestaurant(restaurantId),
+      builder: (context, snapshot) {
+        if (!snapshot.hasData) {
           return Scaffold(
-            appBar: AppBar(
-              title: Text(restaurant.restaurantName),
-              backgroundColor: Colors.indigo[400],
-            ),
-            body: Column(
-              children: [
-                Container(
-                  height: 250,
-                  decoration: BoxDecoration(
-                    image: DecorationImage(
-                      fit: BoxFit.cover,
-                      image:
-                          AssetImage('assets/images/${restaurant.mainImage}'),
+              appBar: AppBar(
+                backgroundColor: Colors.indigo[400],
+              ),
+              body: const Center(child: CircularProgressIndicator()));
+        }
+
+        final restaurant = snapshot.data!;
+
+        return Scaffold(
+          appBar: AppBar(
+            title: Text(restaurant.restaurantName),
+            backgroundColor: Colors.indigo[400],
+          ),
+          body: Column(
+            children: [
+              Container(
+                height: 250,
+                decoration: BoxDecoration(
+                  image: DecorationImage(
+                    fit: BoxFit.cover,
+                    image: AssetImage('assets/images/${restaurant.mainImage}'),
+                  ),
+                ),
+              ),
+              const Gap(20),
+              Column(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Container(
+                    padding: const EdgeInsets.symmetric(horizontal: 12),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        const Icon(Icons.restaurant),
+                        const Gap(20),
+                        Text(
+                          restaurant.description,
+                          style: Styles.textStyle
+                              .copyWith(color: Colors.grey, fontSize: 14),
+                        ),
+                      ],
                     ),
                   ),
-                ),
-                const Gap(20),
-                const Icon(Icons.restaurant),
-                const Gap(20),
-                Container(
-                  padding: const EdgeInsets.symmetric(horizontal: 10),
-                  child: Text(
-                    restaurant.description,
-                    style: Styles.textStyle,
-                    textAlign: TextAlign.center,
+                  const Gap(40),
+                  Container(
+                    height: AppLayout.getHeight(200),
+                    padding: const EdgeInsets.symmetric(horizontal: 10),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      children: [
+                        DetailsRow(
+                          stringText: restaurant.address,
+                          icon: Icon(
+                            Icons.location_on,
+                            color: Styles.blueMarine,
+                          ),
+                        ),
+                        const Gap(20),
+                        DetailsRow(
+                          stringText: restaurant.chefName,
+                          icon: Icon(
+                            Icons.person,
+                            color: Styles.blueMarine,
+                          ),
+                        ),
+                        const Gap(20),
+                        DetailsRow(
+                          stringText: restaurant.webPage,
+                          icon: Icon(
+                            Icons.web,
+                            color: Styles.blueMarine,
+                          ),
+                        ),
+                        const Gap(20),
+                        DetailsRow(
+                          stringText: restaurant.reservations,
+                          icon: Icon(
+                            Icons.phone,
+                            color: Styles.blueMarine,
+                          ),
+                        ),
+                      ],
+                    ),
                   ),
-                ),
-                const Gap(40),
-                DetailsRow(
-                  stringText: restaurant.address,
-                  icon: Icon(
-                    Icons.location_on,
-                    color: Styles.blueMarine,
-                  ),
-                ),
-                const Gap(20),
-                DetailsRow(
-                  stringText: restaurant.chefName,
-                  icon: Icon(
-                    Icons.person,
-                    color: Styles.blueMarine,
-                  ),
-                ),
-                const Gap(20),
-                DetailsRow(
-                  stringText: restaurant.webPage,
-                  icon: Icon(
-                    Icons.web,
-                    color: Styles.blueMarine,
-                  ),
-                ),
-                const Gap(20),
-                DetailsRow(
-                  stringText: restaurant.reservations,
-                  icon: Icon(
-                    Icons.phone,
-                    color: Styles.blueMarine,
-                  ),
-                ),
-              ],
-            ),
-          );
-        });
+                ],
+              ),
+            ],
+          ),
+        );
+      },
+    );
   }
 }
