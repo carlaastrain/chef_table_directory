a12165e240824d2485d80af1202931f1b32f8f22
Carla Astrain <carlaastrain@gmail.com>
2023-01-24 10:29:26 -0800

    fixing android markers size

--- lib/services/custom_marker_service.dart
+++ lib/services/custom_marker_service.dart
@@ -1,5 +1,9 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'dart:ui' as ui;
 
 class CustomMarkerService {
   static final paths = {
@@ -15,14 +19,30 @@ class CustomMarkerService {
 
   CustomMarkerService({required this.markers});
 
+  static Future<Uint8List> getBytesFromAsset(String path, int width) async {
+    ByteData data = await rootBundle.load(path);
+    ui.Codec codec = await ui.instantiateImageCodec(data.buffer.asUint8List(),
+        targetWidth: width);
+    ui.FrameInfo fi = await codec.getNextFrame();
+    return (await fi.image.toByteData(format: ui.ImageByteFormat.png))!
+        .buffer
+        .asUint8List();
+  }
+
   static Future<CustomMarkerService> init() async {
     final markers = <String, BitmapDescriptor>{};
 
     for (var entry in paths.entries) {
-      markers[entry.key] = await BitmapDescriptor.fromAssetImage(
-        const ImageConfiguration(),
-        entry.value,
-      );
+      if (Platform.isAndroid) {
+        final Uint8List markerIcon = await getBytesFromAsset(entry.value, 100);
+        markers[entry.key] = BitmapDescriptor.fromBytes(markerIcon);
+      } else {
+        markers[entry.key] = await BitmapDescriptor.fromAssetImage(
+          const ImageConfiguration(),
+          entry.value,
+          mipmaps: false,
+        );
+      }
     }
 
     return CustomMarkerService(markers: markers);
